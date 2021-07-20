b7bf342c635c66de3bcaaf072c153e192bb65f02
an9513 <angsuo91@gmail.com>
2021-07-20 20:44:50 +0200

    Added sqlite3 & typeorm, need entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [RestaurantsModule],
+  imports: [RestaurantsModule, GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }),
+  TypeOrmModule.forRoot({
+    type:'sqlite',
+    database:'nested-db',
+    entities:['dist/**/*.entity{.ts,.js}'],
+    synchronize:true
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
