5891d7f26677bfee6bcba471dfea2cb4e14c9be0
an9513 <angsuo91@gmail.com>
2021-07-22 20:20:01 +0200

    Added basic resolvers methods, playground ok

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,7 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantInputs } from './dto/createrestaurant.dto';
+import { Restaurant } from './restaurant.entity';
 import { RestaurantsService } from './restaurants.service';
 
 @Resolver()
 export class RestaurantsResolver {
   constructor(private readonly restaurantsService: RestaurantsService) {}
+
+  @Query(() => [Restaurant])
+  getAllRestaurants(){
+    return this.restaurantsService.findAll()
+  }
+
+  @Mutation(() => Restaurant)
+  createRestaurant(@Args("createRestaurantInputs") createRestaurantInputs: CreateRestaurantInputs){
+    return this.restaurantsService.create(createRestaurantInputs);
+  }
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateRestaurantInputs } from './dto/createrestaurant.dto';
+import { Restaurant } from './restaurant.entity';
 
 @Injectable()
-export class RestaurantsService {}
+export class RestaurantsService {
+    constructor(@InjectRepository(Restaurant) private restaurantsRepo: Repository<Restaurant>) {}
+
+    async findAll(): Promise<Restaurant[]> {
+        return this.restaurantsRepo.find();
+    }
+
+    async create(createRestaurantInput: CreateRestaurantInputs): Promise<Restaurant>{
+        const newResto = this.restaurantsRepo.create(createRestaurantInput);
+        return this.restaurantsRepo.save(newResto);
+    }
+}
