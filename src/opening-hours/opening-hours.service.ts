bed0f016091863e6e6bf4d775741a58287cde3cf
an9513 <angsuo91@gmail.com>
2021-07-29 20:44:38 +0200

    Started to add OpeningHours services

--- src/opening-hours/opening-hours.service.ts
+++ src/opening-hours/opening-hours.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OpeningHours } from './opening-hours.entity';
 
 @Injectable()
-export class OpeningHoursService {}
+export class OpeningHoursService {
+    constructor(@InjectRepository(OpeningHours) private openingHoursRepo: Repository<OpeningHours>){}
+
+    async getAll(): Promise<OpeningHours[]>{
+        return await this.openingHoursRepo.find();
+    }
+}
