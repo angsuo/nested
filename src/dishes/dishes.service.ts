7aaaba8e1ab8dbc1be46fb624cd2e0498c58cda6
an9513 <angsuo91@gmail.com>
2021-07-31 19:13:58 +0200

    Added some dish service methods

--- src/dishes/dishes.service.ts
+++ src/dishes/dishes.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Dish } from './dish.entity';
+import { CreateDishInput } from './dto/createDish.dto';
 
 @Injectable()
-export class DishesService {}
+export class DishesService {
+    constructor(@InjectRepository(Dish) private dishRepository: Repository<Dish>){}
+
+    async getAll(): Promise<Dish[]>{
+        return await this.dishRepository.find();
+    }
+
+    async create(createDishInput: CreateDishInput): Promise<Dish>{
+        const newDish = this.dishRepository.create(createDishInput)
+
+        return await this.dishRepository.save(newDish);
+    }
+}
